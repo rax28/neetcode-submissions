28e969c204689bcaf0a61a5ecf88452529482c65
Ravi Balaji <169115820+rax28@users.noreply.github.com>
2026-08-27 07:42:08 +0530

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String minWindow(String s, String t) 
+    {
+        HashMap<Character,Integer> countT = new HashMap<>();
+        HashMap<Character,Integer> window= new HashMap<>();
+
+        int [] res={-1,-1};
+        int reslen=Integer.MAX_VALUE;
+        int l=0;
+        
+        for(int i=0;i<t.length();i++)
+        {
+            countT.put(t.charAt(i),countT.getOrDefault(t.charAt(i),0)+1);
+        }
+
+        int have=0;
+        int need= countT.size();
+
+        for(int r = 0;r<s.length();r++)
+        {
+            char right=s.charAt(r);
+
+            window.put(right,window.getOrDefault(right,0)+1);
+
+            if(countT.containsKey(right)&&window.get(right).equals(countT.get(right)))
+            {
+                have ++;
+
+            }
+
+            while(have==need)
+            {
+                if(r-l+1<reslen)
+                {
+                    reslen=r-l+1;
+                    res[0]=l;
+                    res[1]=r;
+                }
+                char left = s.charAt(l);
+                window.put(left,window.get(left)-1);
+                if(countT.containsKey(left)&&window.get(left)<countT.get(left))
+                {
+                    have--;
+                }
+                l++;
+            }
+
+        }
+        return reslen=Integer.MAX_VALUE ? "":s.substring(res[0],res[1]+1);
+    }
+}
