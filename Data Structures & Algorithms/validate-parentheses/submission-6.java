37313339647fd030b381a1a87da13ef95a4089cd
Ravi Balaji <169115820+rax28@users.noreply.github.com>
2026-08-27 11:29:12 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+      Stack <Character> st = new Stack<>();
+      HashMap<Character,Character> map = new HashMap<>();
+      map.put('}','{');
+      map.put(')','(');
+      map.put(']','[');
+
+        for(char c :s.toCharArray())
+        {
+            if(c=='{'||c=='('||c=='[')
+            {
+                st.push(c);
+            }
+            else if((!st.isEmpty())&&(map.get(c)=st.peek()))
+            {
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    
+    }
+}
